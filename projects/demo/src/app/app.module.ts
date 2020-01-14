23df2b9c6880a7c8ace2fb1d24e964042f858bd2
Roman Szedzielorz <roman.szedzielorz@applandeo.com>
2020-01-14 11:06:25 +0100

    Removed HttpClientModule dependency from the GalleryModule

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -38,7 +37,6 @@ export function hljsLanguages() {
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     AppRoutingModule,
     MaterialModule,
     FlexLayoutModule,
